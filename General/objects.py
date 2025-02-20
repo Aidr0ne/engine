be49127d2d7f0877b02333c474e1b41d1c6ee6fa
Aidr0ne <aidrobin2@gmail.com>
2025-02-20 12:39:59 +0000

    added door

--- General/objects.py
+++ General/objects.py
@@ -1,7 +1,7 @@
 from ._object import Object
 from .camera import Camera
 import pygame as pg
-
+import pickle
 
 class Player:
     def __init__(self, x=0, y=0, physics=False, sprite=None):
@@ -25,14 +25,16 @@ class Player:
         return [
             self.name,
             self.save_id,
+            self.sprite_path,
             self.x,
             self.y,
-            self.sprite_path,
             self.dt,
             self.input_delay,
             self.d,
             self.physics,
         ]
+    def meta(self):
+        return ["Name", "Save Id", "Sprite Path", "x", "y", "dt", "input Delay", "Amount of dt passed For input check", "If physics are enabled"]
 
     def load(self, args):
         self.name = args[0]
@@ -88,7 +90,7 @@ class Player:
         self.dt = self.camera.dt
         return running
 
-    def move(self, grid) -> bool:
+    def move(self, grid):
         running = True
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -129,8 +131,12 @@ class Player:
                     grid[self.x][self.y + 1].hp -= 1
                     if grid[self.x][self.y + 1].hp == 0:
                         grid[self.x][self.y + 1] = Air()
+
+            elif keys[pg.K_SPACE]:
+                grid, self.x, self.y = grid[self.x][self.y].use(grid, self.x, self.y)
+
             self.d = 0
-        return running
+        return running, grid
 
 
 class Air(Object):
@@ -156,11 +162,17 @@ class Air(Object):
         self.hp -= 1
 
     def load(self, args):
-        self.name, self.sprite_path, self.hp, self.transparent, self.solid = args
+        self.name, self.save_id, self.sprite_path, self.hp, self.transparent, self.solid = args
         self.__setstate__()
 
     def save(self):
-        return [self.name, self.sprite_path, self.hp, self.transparent, self.solid]
+        return [self.name, self.save_id, self.sprite_path, self.hp, self.transparent, self.solid]
+    
+    def meta(self):
+        return ["Name", "Save Id", "Sprite Path", "Hp", "Transparent", "Solid"]
+    
+    def use(self, level, x, y):
+        return level, x, y
 
 
 class Test(Object):
@@ -172,10 +184,13 @@ class Test(Object):
         self.hp = 0
 
     def save(self):
-        return [self.name, self.hp, self.solid, self.transparent, self.sprite_path]
+        return [self.name, self.save_id, self.sprite_path, self.hp, self.solid, self.transparent]
+    
+    def meta(self):
+        return []
 
     def load(self, args):
-        self.name, self.hp, self.solid, self.transparent, self.sprite_path = args
+        self.name, self.save_id, self.sprite_path, self.hp, self.solid, self.transparent = args
         self.__setstate__()
 
     def hit(self):
@@ -194,6 +209,9 @@ class Test(Object):
             self.image = None
             print("sprite load fail")
 
+    def use(self, level, x, y):
+        return level, x, y
+
 
 class Ground(Object):
     def __init__(self, x=0, y=0, solid=True, transparent=False, sprite=None, health=5):
@@ -204,8 +222,8 @@ class Ground(Object):
         self.sprite_path = sprite
 
     def save(self):
-        return [
-            self.name,
+        return [self.name, 
+            self.save_id,
             self.sprite_path,
             self.hp,
             self.solid,
@@ -213,10 +231,14 @@ class Ground(Object):
             self.x,
             self.y,
         ]
+    
+    def meta(self):
+        return ["Name", "Save Id", "Sprite Path", "Hp", "Solid", "Transparent", "X", "Y"]
 
     def load(self, args):
         (
             self.name,
+            self.save_id,
             self.sprite_path,
             self.hp,
             self.solid,
@@ -241,25 +263,59 @@ class Ground(Object):
 
     def health(self):
         return self.hp
+    
+    def use(self, level, x, y):
+        return level, x, y
 
 
 class Door:
-    def __init__(self, x, y, level, tx, ty, sprite_path, locked=False):
+    def __init__(self, name="Door", x=0, y=0, tx=0, ty=0, target_file="", sprite_path="", save_dict=None, locked=False):
         self.x = x
         self.y = y
-        self.level = level
         self.tx = tx
         self.ty = ty
         self.transparent = True
+        self.target_file = target_file
         self.hp = 1000000000
         self.solid = False
         self.locked = locked
         self.sprite_path = sprite_path
         self.image = None
+        self.save_id = 4
+        self.name = name
+        self.save_dict = save_dict
 
-    def enter(self):
+    def save(self):
+        return [self.name, self.save_id, self.sprite_path, self.x, self.y, self.tx, self.ty, self.locked, self.hp, self.target_file, self.save_dict]
+    
+    def meta(self):
+        return ["Name", "Save Id", "Sprite Path", "X", "Y", "Target X", "Target Y", "Locked", "Hp", "Target File", "Save dictionary"]
+    
+    def load(self, args):
+        self.name, self.save_id, self.sprite_path, self.x, self.y, self.tx, self.ty, self.locked, self.hp, self.target_file, self.save_dict = args
+        self.__setstate__()
+
+    def use(self, level, x, y):
+        if not self.locked:
+            path = self.target_file
+            save_dict = self.save_dict
+            with open(path, "rb") as file:
+                data = pickle.load(file)
+            level = [[None for _ in range(len(data))] for _ in range(len(data[0]))]
+            for x in range(len(data)):
+                for y in range(len(data[0])):
+                    level[x][y] = save_dict[data[x][y][0]]
+                    print(data[x][y])
+                    i = save_dict[data[x][y][0]]()
+                    i.load(data[x][y])
+                    level[x][y] = i
+
+            return level, self.tx, self.ty
+        return level, x, y
+
+    def __setstate__(self):
         if self.sprite_path is not None:
-            self.image = pg.image.load(self.sprite_path)
+            self.image = pg.image.load(self.sprite_path)  # Player sprite
         else:
             self.image = None
-            print("Door load fail")
+            print("sprite load fail")
--- General/save.py
+++ General/save.py
@@ -3,12 +3,17 @@ import pickle
 
 def save_level(path, level):
     data = [[None for _ in range(len(level))] for _ in range(len(level[0]))]
+    meta = [[None for _ in range(len(level))] for _ in range(len(level[0]))]
     for x in range(len(level)):
         for y in range(len(level[0])):
             data[x][y] = level[x][y].save()
+            meta[x][y] = level[x][y].meta()
 
     with open(path, "wb") as file:
         pickle.dump(data, file)
+    meta_path = path + ":meta"
+    with open(meta_path, "wb") as file:
+        pickle.dump(meta, file)
 
 
 def load_level(path, save_dict):
